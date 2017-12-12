957d09234dd31da37bfbbb65a46e5cfff6436066
Suraj Nayak <1suraj.rims@gmail.com>
2017-12-12 21:48:37 +0530

    NE Changes

--- app.py
+++ app.py
@@ -2,11 +2,14 @@
 
 
 import numpy as np
-from flask import Flask, abort, jsonify, request
+from flask import Flask, abort, jsonify, request,render_template
+from textblob import TextBlob
+from textblob.sentiments import NaiveBayesAnalyzer
 #from flask_cors import cross_origin
 #from sklearn.externals import joblib
 #from json import dumps
-import _pickle as pickle 
+import _pickle as pickle
+
 
 app = Flask(__name__)
 
@@ -15,8 +18,8 @@ SC= pickle.load(open('SC_X.pkl','rb'))
 
 @app.route('/api', methods=['POST'])
 def make_predict():
-    
-    # This Cn be requested through 
+
+    # This Cn be requested through
     '''
     import requests, json
     url = 'http://localhost:5000/api1'
@@ -25,7 +28,7 @@ def make_predict():
 
     print(r.text)
     '''
-    
+
     data = request.get_json(force=True)
     Age = data['Age']
     Salary = data['Salary']
@@ -37,23 +40,35 @@ def make_predict():
     if output==0:
         prediction='Not purchases'
     else: prediction='purchase'
-   
-    
+
+
     #return test
     return jsonify(prediction)
     #return jsonify(results =a)
 
 @app.route('/')
 def hello_world():
-    return 'I love you Deepika'
-'''
-@app.route('/api', methods=['POST'])
-def hello_world1():
-    data = request.get_json(force=True)
-    data = data['no']
-    return data
+    return 'Deployment Successful!!!!!'
 
-'''    
+
+
+@app.route('/LiveSentiment')
+def my_form():
+    return render_template('Suraj.html')
+
+
+@app.route('/LiveSentiment', methods=['POST'])
+def my_form_post():
+    text = request.form['text']
+    blob = TextBlob(text, analyzer=NaiveBayesAnalyzer())
+    processed_text = blob.sentiment.classification
+    #processed_text = str(processed_text)
+    return render_template('Suraj.html',value=processed_text)
+
+
+
+
+
+
 if __name__ == '__main__':
     app.run()
-    
